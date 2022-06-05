3e5d68694493dc336571beb7b0bcb5b2591a159e
Talip <talipakcelik@gmail.com>
2022-06-05 08:50:58 +0300

    Updates

---
+++ precache-manifest.edbbfa6a6118b7778216f68c934ad4f6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "30c579482d8cc92491ea",
+    "url": "/travel-journey/static/js/main.30c57948.chunk.js"
+  },
+  {
+    "revision": "4647fec68490891e2555",
+    "url": "/travel-journey/static/js/1.4647fec6.chunk.js"
+  },
+  {
+    "revision": "3c37cc8fca5a84e250c3",
+    "url": "/travel-journey/static/js/runtime~main.3c37cc8f.js"
+  },
+  {
+    "revision": "4b0d0cfee567acae5dd620aa7fa1d2d6",
+    "url": "/travel-journey/index.html"
+  }
+];
---
+++ static/js/main.30c57948.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],[,,,function(e,n,o){e.exports=o(4)},function(e,n,o){"use strict";o.r(n);var t=o(0),r=o.n(t),c=o(2);o.n(c).a.render(r.a.createElement("h1",null,"Hello, world!"),document.getElementById("root"))}],[[3,2,1]]]);
+//# sourceMappingURL=main.30c57948.chunk.js.map
